bf7cf1fdc8a176a5fb0451737fe651bbd73d06e2
Guggenberger <github2022@guggenberger.me>
2023-01-14 19:23:56 +0100

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,80 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 
+/////////////////////////////////////////////////////////////////////////// TFT Konfig
+#define TFT_CS         4
+#define TFT_RST        16                                            
+#define TFT_DC         5
+// Color definitions
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
+#define BITCOIN 0xFD20
+
+//#define COLOR3 ST7735_YELLOW
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+/////////////////////////////////////////////////////////////////////////// Intervall der Steuerung
+unsigned long previousMillis_btckurs = 0;
+unsigned long interval_btckurs = 15000; 
+
+
+/////////////////////////////////////////////////////////////////////////// Funktionsprototypen
+//void callback                (char*, byte*, unsigned int);
+void loop                      ();
+void setup                      ();
+void tft_text                  (int x, int y, int size, char *text, uint16_t color);
+
+
+//**************************************************************************** void SETUP
 void setup() {
-  // put your setup code here, to run once:
+
+  /////////////////////////////////////////////////////////////////////////// TFT initialisieren
+  tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
+  
+  // TFT einfärben
+  tft.fillScreen(BLACK);
+
+  // Serielle Kommunikation starten
+  Serial.begin(115200);
+
+
+    // TFT einfärben
+   tft.fillScreen(BLACK);
+   tft.setCursor(13,30);
+   tft.setTextColor(RED,BLACK);
+   tft.setTextSize(1);
+   tft.print("Start");
+
+   delay(1500);
+
 }
 
+/////////////////////////////////////////////////////////////////////////// TFT - Text schreiben
+void tft_text(int x, int y, int size, char *text, uint16_t color) {
+  tft.setTextWrap(true);
+  tft.setCursor(x, y);
+  tft.setTextColor(color);
+  tft.setTextSize(size);
+  tft.print(text);
+}
+
+/////////////////////////////////////////////////////////////////////////// VOID LOOP
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  ///////////////////////////////////////////////////////////////////////// BTC Kurs abfragen
+  if (millis() - previousMillis_btckurs > interval_btckurs) {
+      previousMillis_btckurs = millis();   // aktuelle Zeit abspeichern
+      // BTC Kurs abfragen
+
+    }
+
+
+
 }
