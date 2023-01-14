47d9129c71b57c5ef4db995bf2ef344ec99428ec
Guggenberger <github2022@guggenberger.me>
2023-01-14 21:30:17 +0100

    init

---
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ /home/chavilli/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /home/chavilli/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,5 @@
 #include <Arduino.h>
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
-
-/////////////////////////////////////////////////////////////////////////// TFT Konfig
-#define TFT_CS         4
-#define TFT_RST        16                                            
-#define TFT_DC         5
-// Color definitions
-#define BLACK 0x0000
-#define BLUE 0x001F
-#define RED 0xF800
-#define GREEN 0x07E0
-#define CYAN 0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW 0xFFE0
-#define WHITE 0xFFFF
-#define BITCOIN 0xFD20
-
-//#define COLOR3 ST7735_YELLOW
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+#include <TFT_eSPI.h> 
 
 /////////////////////////////////////////////////////////////////////////// Intervall der Steuerung
 unsigned long previousMillis_btckurs = 0;
@@ -36,22 +17,12 @@ void elektrolyse_umkehren      ();
 //**************************************************************************** void SETUP
 void setup() {
 
-  /////////////////////////////////////////////////////////////////////////// TFT initialisieren
-  tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
-  
-  // TFT einfärben
-  tft.fillScreen(BLACK);
+
 
   // Serielle Kommunikation starten
   Serial.begin(115200);
 
 
-    // TFT einfärben
-   tft.fillScreen(BLACK);
-   tft.setCursor(13,30);
-   tft.setTextColor(RED,BLACK);
-   tft.setTextSize(1);
-   tft.print("Start");
 
    delay(1500);
 
@@ -59,11 +30,7 @@ void setup() {
 
 /////////////////////////////////////////////////////////////////////////// TFT - Text schreiben
 void tft_text(int x, int y, int size, char *text, uint16_t color) {
-  tft.setTextWrap(true);
-  tft.setCursor(x, y);
-  tft.setTextColor(color);
-  tft.setTextSize(size);
-  tft.print(text);
+
 }
 
 /////////////////////////////////////////////////////////////////////////// Elektrolyse
